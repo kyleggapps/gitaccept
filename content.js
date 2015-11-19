5415e0ed57aadb246ad973a8294dff2e527775ec
Sam Cambridge <sam@ggapps.co.uk>
2015-11-19 11:22:41 +0000

    removes the console.logs

--- content.js
+++ content.js
@@ -1,7 +1,4 @@
 var thumbs = $('img[alt=":+1:"]');
-
-console.log(thumbs);
-
 var top_menu = $('ul.pagehead-actions')
 
 top_menu.append('<li><a href="#accept-box" class="btn btn-sm btn-primary btn-with-count" title="Accept"><span class="octicon octicon-check"></span>Approve</a><a href="#" class="social-count">'+thumbs.length+'</a></li>');
@@ -13,7 +10,6 @@ $('img[alt=":+1:"]').each(function(){
 
 $('a[href="#accept-box"]').on('click',function(e){
   e.preventDefault();
-  console.log('clicked');
   $('#new_comment_field').text(':+1:');
   $('.js-new-comment-form').submit();
 });
